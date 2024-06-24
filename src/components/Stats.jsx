70bb9805552410a63b5601ed3c8e5ee350577e02
DaisyEverard <daisy.everard@bjss.com>
2024-06-24 20:17:45 +0100

    add reminingPoints

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,11 +1,22 @@
 import HideableDescriptionCell from "../utils/HideableDescriptionCell";
 import statsJson from "../data/statsDescription.json"
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { StatsContext } from "../App";
 import IncrementDecrementStat from "../utils/IncrementDecrementStat";
 
 export default function Stats() {
     const {stats, setStats} = useContext(StatsContext)
+    const [remainingPoints, setRemainingPoints] = useState(42)
+
+    const getUsedPoints = (stats) => {
+        let total = 0
+        for (const key in stats) {
+            const value = stats[key]
+            total += parseInt(value)
+        }
+        return total
+        
+    }
 
     return (
         <div className="flexRow">
@@ -24,6 +35,10 @@ export default function Stats() {
             )}
             </div>
             <div className="flexCol" style={{width: "50vw", flex: 1}}>
+            <div>
+                <p><strong>Points Remaining:</strong></p>
+                <p>{remainingPoints}</p>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -40,7 +55,7 @@ export default function Stats() {
                             <tr key={index}>
                                 <th>{statName}</th>
                                 <td>{level}</td>
-                                <IncrementDecrementStat statName={statName}/>
+                                <IncrementDecrementStat statName={statName} setRemainingPoints={setRemainingPoints} remainingPoints={remainingPoints}/>
                             </tr>
                             )
                         })
--- src/utils/IncrementDecrementStat.jsx
+++ src/utils/IncrementDecrementStat.jsx
@@ -3,23 +3,30 @@ import { StatsContext } from "../App"
 
 export default function IncrementDecrementStat(props) {
     let statName = props.statName
+    const remainingPoints = props.remainingPoints
+    const setRemainingPoints = props.setRemainingPoints
+
     const {stats, setStats} = useContext(StatsContext)
 
-    const increment = (stats, statName) => {
-        const newStats = {...stats}
+    const increment = (stats, statName, remainingPoints, setRemainingPoints) => {
+        if (remainingPoints > 0 && stats[statName] < 8) {
+            const newStats = {...stats}
         newStats[statName] += 1
         setStats(newStats)
+        setRemainingPoints(remainingPoints - 1)
+        }
     }
-    const decrement = (stats, statName) => {
+    const decrement = (stats, statName, remainingPoints, setRemainingPoints) => {
         if (stats[statName] > 2) {
             const newStats = {...stats}
         newStats[statName] -= 1
         setStats(newStats)
+        setRemainingPoints(remainingPoints + 1)
         }
     }
 
     return (<td>
-        <button onClick={(e)=> {increment(stats, statName)}}>+</button>
-        <button onClick={(e)=> {decrement(stats, statName)}}>-</button>
+        <button onClick={(e)=> {increment(stats, statName, remainingPoints, setRemainingPoints)}}>+</button>
+        <button onClick={(e)=> {decrement(stats, statName, remainingPoints, setRemainingPoints)}}>-</button>
     </td>)
 }
