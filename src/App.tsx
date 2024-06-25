86247a8e8e1deba6b0a36c04d41c1c317033157f
George Hanks <george.hanks@bjss.com>
2024-06-25 14:19:27 +0100

    Convert To TS

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './style/index.css';
 
 import defaultStats from "./data/defaultStats.json"
 
-import { calculateHP } from "./utils/commonMethods.jsx"
+import { calculateHP } from "./utils/commonMethods"
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
