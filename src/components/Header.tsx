d2d3a54de699f0a2c2181ac5689e86ae07fc5ed8
DaisyEverard <117744366+DaisyEverard@users.noreply.github.com>
2024-06-28 11:24:08 +0100

    Feature/style modals (#8)
    * create skills.json
    
    * add comment to skills.tsx
    
    * add template for skills table
    
    * filter skills and add to tables
    
    * remove redundant import
    
    * add style fo rtables
    
    * add styling to stats
    
    * create effects.json
    
    * add SVGs for effect bar
    
    * fix margins and spacing on effects symbols
    
    * add modal logic to effects.tsx
    
    * add basic modal styling
    
    * add tailwind to package-lock.json
    
    * add styling to modal
    
    ---------
    
    Co-authored-by: DaisyEverard <daisy.everard@bjss.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 const Header = () => {
-  return <div className="flex h-14 items-center space-x-6 border-b px-5">Cyberpunk Red - Character Assistant</div>;
+  return <div id="header" 
+  className="bg-main-dark-bg text-main-light-text flex h-14 items-center space-x-6 border-b px-5">Cyberpunk Red - Character Assistant</div>;
 };
 
 export default Header;
--- src/utils/ModalForMapState.tsx
+++ src/utils/ModalForMapState.tsx
@@ -6,17 +6,22 @@ const SimpleModal = (props) => {
     let modalDisplay = props.modalDisplays
     const setModalDisplay = props.setModalDisplays
     const toggleModalDisplays = props.toggleModalDisplays
+    const iconMap = props.iconMap
     
     return (
         <div style={{display: modalDisplay[key]}}>
-        <div className='modalBG'>
+        <div className='modalBG' onClick={(e) => {toggleModalDisplays(modalDisplay, setModalDisplay, key)}}>
         </div>
+        <div className="modalContainer">
         <div className="modal flexCol">
-        <button onClick={(e) => {toggleModalDisplays(modalDisplay, setModalDisplay, key)}}>X</button>
-        <h2>{key}</h2>
+        <button onClick={(e) => {toggleModalDisplays(modalDisplay, setModalDisplay, key)}} className="closeModalButton">X</button>
+        <img 
+          src={iconMap[key]} 
+          alt={key["alt"]}
+          style={{height: "2rem"}}></img>
+        <h2>{key.toUpperCase()}</h2>
         <p>{content}</p>
-        </div></div>
-
+        </div></div></div>
     )
 }
 
