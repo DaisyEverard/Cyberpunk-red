a80ba0742663fad0ecc35409d051191793f7c1ab
George Hanks <george.hanks@bjss.com>
2024-06-28 09:58:25 +0100

    Convert Header & Footer To Tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,5 @@
 const Header = () => {
-  return (
-    <div id="header">
-      <h2>This is the header</h2>
-    </div>
-  );
+  return <div className="flex h-14 items-center space-x-6 border-b px-5">Cyberpunk Red - Character Assistant</div>;
 };
 
 export default Header;
