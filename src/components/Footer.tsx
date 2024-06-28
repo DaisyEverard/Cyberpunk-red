075a4c3be58044e2d40c54376337706f488a481b
DaisyEverard <117744366+DaisyEverard@users.noreply.github.com>
2024-06-28 13:05:05 +0100

    Bug/expand buttons not showing +or  (#10)
    * convert some button properties to tailwind
    
    * convert button font-weight and wrap to tailwind
    
    * move overflow-y: scross from .modalContainer to .modal
    
    * remove footer test
    
    * add HideableDescriptionCellProps
    
    ---------
    
    Co-authored-by: DaisyEverard <daisy.everard@bjss.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div id="footer"className="flex h-14 items-center space-x-6 border-t px-5">footer</div>;
+  return <div id="footer"className="flex h-14 items-center space-x-6 border-t px-5"></div>;
 };
 
 export default Footer;
