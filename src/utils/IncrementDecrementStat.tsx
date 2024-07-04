6ec181be10a92b3d0b43cbc4baf729da31219ef1
DaisyEverard <117744366+DaisyEverard@users.noreply.github.com>
2024-07-04 14:29:27 +0100

    Bug/will and body stat changes resetting hp (#22)
    * update todo
    
    * Use Stats type
    
    * update HP on stat change only if it's above max
    
    * update todo
    
    ---------
    
    Co-authored-by: DaisyEverard <daisy.everard@bjss.com>

--- src/utils/IncrementDecrementStat.tsx
+++ src/utils/IncrementDecrementStat.tsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
 
 import { HPContext, HumanityContext, StatsContext } from '../App';
-import { updateHP, updateHumanity } from './commonMethods';
+import { Stats } from '../types/types';
+import { calculateHPMax, updateHumanity } from './commonMethods';
 
 type IncrementDecrementStatProps = {
   statName: string;
@@ -15,7 +16,7 @@ const IncrementDecrementStat = ({ statName, remainingPoints, setRemainingPoints 
   const { humanity, setHumanity } = useContext(HumanityContext);
 
   const increment = (
-    stats: Record<string, number>,
+    stats: Stats,
     statName: string,
     remainingPoints: number,
     setRemainingPoints: (points: number) => void,
@@ -25,25 +26,31 @@ const IncrementDecrementStat = ({ statName, remainingPoints, setRemainingPoints 
       newStats[statName] += 1;
 
       setStats(newStats);
-      updateHP(setHP, newStats);
       updateHumanity(setHumanity, newStats);
       setRemainingPoints(remainingPoints - 1);
     }
   };
   const decrement = (
-    stats: Record<string, number>,
+    stats: Stats,
     statName: string,
     remainingPoints: number,
     setRemainingPoints: (points: number) => void,
+    HP: number,
+    setHP: (HP: number) => void,
+    calculateHPMax: (stats: Stats) => number,
   ) => {
     if (stats[statName] > 2) {
       const newStats = { ...stats };
       newStats[statName] -= 1;
 
       setStats(newStats);
-      updateHP(setHP, newStats);
       updateHumanity(setHumanity, newStats);
       setRemainingPoints(remainingPoints + 1);
+
+      const HPMax = calculateHPMax(newStats);
+      if (HP > HPMax) {
+        setHP(HPMax);
+      }
     }
   };
 
@@ -60,7 +67,7 @@ const IncrementDecrementStat = ({ statName, remainingPoints, setRemainingPoints 
       <button
         onClick={e => {
           e.preventDefault();
-          decrement(stats, statName, remainingPoints, setRemainingPoints);
+          decrement(stats, statName, remainingPoints, setRemainingPoints, HP, setHP, calculateHPMax);
         }}
       >
         -
