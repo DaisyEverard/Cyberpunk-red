cd8fc960a76985de0e8d1dca279f049dfd1b80d1
DaisyEverard <daisy.everard@bjss.com>
2024-07-20 20:10:16 +0100

    add Header.test.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
-    <div
+    <h1
       id="header"
       className="bg-main-dark-bg text-main-light-text flex items-center px-4 py-2"
     >
       Cyberpunk Red - Character Assistant
-    </div>
+    </h1>
   );
 };
 
