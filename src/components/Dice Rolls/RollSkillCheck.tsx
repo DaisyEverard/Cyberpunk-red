fc65042168c2760e9ce3916115f7c9d4b933683c
DaisyEverard <daisy.everard@bjss.com>
2025-02-27 16:46:22 +0000

    add base_stat for skills

--- src/components/Dice Rolls/RollSkillCheck.tsx
+++ src/components/Dice Rolls/RollSkillCheck.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+
+import { CharacterContext } from '../../context/Character';
 import { rollSkillCheck } from '../../utils/commonMethods';
 
 type RollSkillCheckProps = {
@@ -6,18 +9,22 @@ type RollSkillCheckProps = {
 };
 
 export const RollSkillCheck = ({ skillName, skillLevel }: RollSkillCheckProps) => {
-  const handleButtonOnClick = (skillLevel: number) => {
+  const { getCurrentSkills, getStats } = useContext(CharacterContext);
+  const baseStatType = getCurrentSkills()[skillName]['stat_type'];
+  const statLevel = getStats()[baseStatType];
+
+  const handleButtonOnClick = (skillLevel: number, statLevel: number) => {
     const baseRoll = rollSkillCheck().reduce((x, i) => x + i, 0);
-    console.log(baseRoll + skillLevel);
-    return baseRoll + skillLevel;
-    // return rollSkillCheck() + stat + skill
+    console.log([baseRoll, skillLevel, statLevel]);
+    // need a modal to display
+    return baseRoll + skillLevel + statLevel;
   };
 
   return (
     <div className="flex flex-col gap-2">
       <button
         onClick={e => {
-          handleButtonOnClick(skillLevel);
+          handleButtonOnClick(skillLevel, statLevel);
         }}
         className="rounded-md bg-main-dark-bg px-2 py-1 text-main-light-text shadow hover:bg-main-dark-bg/80"
       >
