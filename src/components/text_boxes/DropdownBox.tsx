96df13a386c63f5cea9245efa667d289034c758d
DaisyEverard <daisy.everard@bjss.com>
2025-03-12 15:06:50 +0000

    remove dropdown from css

--- src/components/text_boxes/DropdownBox.tsx
+++ src/components/text_boxes/DropdownBox.tsx
@@ -10,7 +10,7 @@ const DropdownBox = ({ value, values, onChanged }: DropdownCellProps) => {
   return (
     <div className="dropdown">
       <p className="px-1 py-1 hover:bg-active-bg-grey">{value}</p>
-      <div className="dropdown-content">
+      <div className="dropdown-content absolute bg-[#f1f1f1] min-w-[160px] z-1 shadow-[0px_8px_16px_0px_rgba(0,0,0,0.2)]">
         {values.map(role => {
           return (
             <p
