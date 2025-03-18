89ffa707ddb1d849daf0beee31a9d8e375988d6c
DaisyEverard <daisy.everard@bjss.com>
2025-03-18 09:30:20 +0000

    remove comments, install axios

--- src/context/Character.tsx
+++ src/context/Character.tsx
@@ -42,19 +42,9 @@ export const CharacterFactory = (): CharacterType => {
   };
 };
 
-// You touch this perfectly good function declaration, George, and I'mma come beat yo ass!
-// Fix It For Ya :)
-// Good lad.
-
-// It should go without saying that this is an example of one possible
-// method. Feel free to experiment and find what works for you and
-// your application.
-
-// Disclaimer: This is probably not production ready code.
 export default function CharacterProvider({ children }: PropsWithChildren) {
   const [character, setCharacter] = useState(CharacterFactory());
 
-  // SETTERS
   function setName(name: string) {
     setCharacter(current => ({ ...current, name }));
   }
@@ -78,7 +68,6 @@ export default function CharacterProvider({ children }: PropsWithChildren) {
 
       return { ...current, HP };
     });
-    // set dead and critically wounded here instead
   }
 
   function setHumanity(humanity: number) {
@@ -95,34 +84,16 @@ export default function CharacterProvider({ children }: PropsWithChildren) {
 
   // OTHER
   function someOtherPublicFunction(message: string) {
-    // Obviously you're not limited to just gaving getters and setters.
-    // You can also export other methods (to stick with our "class" anaology) which
-    // anything else you might need a consuming component to do.
-
     aPrivateFunction(message);
   }
 
   function aPrivateFunction(message: string) {
-    // "Private" in that were're just not exporting it.
-    // Good for the nuts and bolts that you don't want to expose to your
-    // comsuming components.
-
-    // If you want things a bit neater you can put it outside this
-    // function and just call it from there, passing in any data
-    // you need it to consume etc.
-
-    // Whatever. Get an egg. Suck it.
-
     console.log(message);
   }
 
   return (
     <CharacterContext.Provider
       value={{
-        // Here we export the methods and variables we want to expose
-        // to the components consuming this context. Anything inside
-        // the provider will be able to access these with the useContext
-        // hook.
         state: character,
         setName,
         setRole,
